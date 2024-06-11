4e08a1269d3b5c8dd7a204fd986ac536113e3948
Marijus <electromenn@gmail.com>
2024-06-11 10:08:34 +0300

    Add redux and a template to see it running

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import { store } from './app/store.ts'
 import App from './App.tsx'
 import About from './pages/About/index.tsx'
 import Contacts from './pages/Contacts/index.tsx'
@@ -28,6 +30,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
