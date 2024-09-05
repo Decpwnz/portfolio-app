7d0e99d96492ff6871bdc60082824b075dca09f1
Marijus <electromenn@gmail.com>
2024-09-05 18:55:22 +0300

    add styles for Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
-import { Counter } from './features/counter/Counter'
+// import { Counter } from './features/counter/Counter'
 
 function App() {
   return (
     <>
       <Outlet /> {/* This renders the matched child route */}
-      <Counter />
+      {/* <Counter /> */}
     </>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { FaLinkedin } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 import styles from './Home.module.css'
@@ -20,18 +21,29 @@ function Home() {
             </li>
           </ul>
         </nav>
-        <div className={styles.themeToggle}>{/* Add theme toggle button here */}GH_Icon</div>
+        <div className={styles.themeToggle}>{/* TODO Github icon or theme toggle button here */}GH</div>
       </header>
       <main className={styles.main}>
         <h1>Hey! I'm Marijuš</h1>
         <p>
           Welcome to my portfolio website. Please explore the different sections to learn more about me and my work.
         </p>
-        <button className={styles.contactButton}>Contact me</button>
-        <div className={styles.socialIcons}>{/* Add social media icons here */}</div>
+        <div className={styles.contactContainer}>
+          <button className={styles.contactButton}>Contact me</button>
+          <div className={styles.socialIcons}>
+            <a
+              className={styles.iconLink}
+              href="https://www.linkedin.com/in/marijus-ladanovski"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
       </main>
       <footer className={styles.footer}>
-        <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Marijuš Ladanovski. All rights reserved.</p>
       </footer>
     </div>
   )
